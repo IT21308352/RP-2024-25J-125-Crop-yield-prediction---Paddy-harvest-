961d563e1c518568def5f8d085b4695000b5091e
IT21308352 <100343943+IT21308352@users.noreply.github.com>
2025-03-16 20:09:26 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import InputPage from './pages/InputPage';
+import ResultPage from './pages/ResultPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import { AuthProvider } from './AuthContext';
+import PrivateRoute from './PrivateRoute';
+import "./App.css";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import PaddyYieldHome from './pages/PaddyYeildHome';
+import FieldPreparationPage from './pages/FieldPreparationPage';
+import SeedSelectionPage from './pages/SeedSelectionPage';
+import CropNurturingPage from './pages/CropNurturingPage';
+import PestControlPage from './pages/PestControlPage';
+import IrrigationManagementPage from './pages/IrrigationManagementPage';
+import HarvestPlanningPage from './pages/HarvestPlaningPage';
+import RecommendationsPage from './pages/RecommendationsPage';
+
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<PaddyYieldHome />} />
+          <Route path="/input-form" element={<InputPage />} />
+          <Route path="/result" element={<ResultPage />} />
+          
+          <Route path="/field-preparation" element={<FieldPreparationPage />} />
+          <Route path="/seed-selection" element={<SeedSelectionPage />} />
+          <Route path="/crop-nurturing" element={<CropNurturingPage />} />
+          <Route path="/pest-control" element={<PestControlPage />} />
+          <Route path="/irrigation-management" element={<IrrigationManagementPage />} />
+          <Route path="/harvest-planning" element={<HarvestPlanningPage />} />
+          <Route path="/recommendations" element={<RecommendationsPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthProvider>
+  );
+};
+
+export default App;
